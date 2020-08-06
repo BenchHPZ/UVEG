f40ddcf13cb00a847cc7275c1fde0b8632ab061b
BenchHPZ <40551190+BenchHPZ@users.noreply.github.com>
2020-08-06 08:18:31 -0700

    Reto 2 de U2 en Taller Base de Datos
    Codigo revisado y probado en mySql worbench

--- TallerDeBasesDeDatos/Actividades/Rivera_Benjamin_TBD_R2_U2.sql
+++ TallerDeBasesDeDatos/Actividades/Rivera_Benjamin_TBD_R2_U2.sql
@@ -7,11 +7,9 @@
 	- Nombre asesor:	Araceli González García 
 */
 
-
 -- 2. Base de datos storepara la empresa Electronic Technologies 
-CREATE DATABASE empresa;
- 
-USE empresa;
+CREATE DATABASE tienda;
+USE tienda;
  
 CREATE TABLE tienda(
  	id 			INT 		 NOT NULL PRIMARY KEY,
@@ -20,7 +18,7 @@ CREATE TABLE tienda(
 );
  
 CREATE TABLE productos(
- 	id 			INT 		NOt NULL PRIMARY KEY,
+ 	id 			INT 		NOT NULL PRIMARY KEY,
  	nombre 		VARCHAR(50) NOT NULL,
  	precio 		DOUBLE 		NOT NULL,
  	descripcion VARCHAR(50) NULL
@@ -42,14 +40,14 @@ CREATE TABLE cliente(
 -- 4. Tabla payment_method
 CREATE TABLE forma_pago(
 	id 			INT 		NOT NULL PRIMARY KEY,
-	forma_pago 	VARCHAR(50)	NOT NULL	
+	forma_pago 	VARCHAR(50)	NOT NULL,
+	id_cliente	INT 		NULL
 );
 
 
 -- 5. Relaciones entre tablas
 ALTER TABLE forma_pago 
-	ADD CONSTRAINT compra 
-	FOREIGN KEY id_cliente 
+	ADD FOREIGN KEY (id_cliente) 
 	REFERENCES cliente(id);
 
 /*La siguiente linea se agregao para poder generar el
@@ -59,20 +57,25 @@ diagrama que se solicita en el Reto.
 	producto, una realcion uno a muchos de tienda a
 	producto
 */
-ALTER TABLE productos
-	ADD CONSTRAINT inventario
-	FOREIGN KEY id_tienda
+ALTER TABLE productos ADD id_tienda INT NOT NULL;
+
+ALTER TABLE productos 
+	ADD FOREIGN KEY (id_tienda)
 	REFERENCES tienda(id);
-
-
+    
+    
 -- 6. Tabla purchase_order
 CREATE TABLE orden_compra(
-	id 				INT		NOT NULL PRIMARY KEY,
-	id_forma_pago 	INT 	NOT NULL FOREIGN KEY
-		REFERENCES forma_pago(id),
-	id_cliente		INT 	NOT NULL FOREIGN KEY
-		REFERENCES cliente(id)
+	id 			INT 		NOT NULL PRIMARY KEY,
+    id_forma_pago 	INT		NOT NULL,
+    id_cliente 		INT		NOT NULL
 );
+ALTER TABLE orden_compra
+	ADD FOREIGN KEY (id_forma_pago)
+	REFERENCES forma_pago(id);
+ALTER TABLE orden_compra
+	ADD FOREIGN KEY (id_cliente)
+	REFERENCES cliente(id);
 
 
 CREATE TABLE detalle_orden_compra(
@@ -80,11 +83,112 @@ CREATE TABLE detalle_orden_compra(
 	cantidad	INT 		NOT NULL,
 	precio_x_unidad DOUBLE	NOT NULL,
 	descripcion VARCHAR(150)	NULL,
-	id_producto INT 		NOT NULL FOREIGN KEY
-		REFERENCES productos(id,
-	id_orden_compra INT 	NOT NULL FOREIGN KEY
-		REFERENCES orden_compra(id)
+    id_producto 	INT 	NOT NULL,
+    id_orden_compra INT 	NOT NULL
 );
+ALTER TABLE  detalle_orden_compra ADD FOREIGN KEY 
+	(id_producto) REFERENCES productos(id);
+ALTER TABLE  detalle_orden_compra ADD FOREIGN KEY 
+	(id_orden_compra) REFERENCES orden_compra(id);
+
+
+-- 7. Revisión de estructura
+/*
+	La estructura resultante del codigo anterior
+	puede ser consultado en 
+	https://github.com/BenchHPZ/UVEG-IngSisComp/tree/master/TallerDeBasesDeDatos/Actividades/assets//*Reto 2. Crear mi primera base de datos
+1. Datos de identificacion
+	- Nombre: 			Benjamin Rivera
+	- Matricula: 		19015478
+	- Fehca elaboracion:6 de Agosto de 2020
+	- Nombre modulo:	Taller de bases de datos v2
+	- Nombre asesor:	Araceli González García 
+*/
+
+
+-- 2. Base de datos storepara la empresa Electronic Technologies 
+CREATE DATABASE tienda;
+USE tienda;
+ 
+CREATE TABLE tienda(
+ 	id 			INT 		 NOT NULL PRIMARY KEY,
+ 	nombre 		VARCHAR(100) NOT NULL,
+ 	direccion 	VARCHAR(100) NULL
+);
+ 
+CREATE TABLE productos(
+ 	id 			INT 		NOT NULL PRIMARY KEY,
+ 	nombre 		VARCHAR(50) NOT NULL,
+ 	precio 		DOUBLE 		NOT NULL,
+ 	descripcion VARCHAR(50) NULL
+);
+
+
+-- 3. Tabla customer
+CREATE TABLE cliente(
+	id 			INT 		NOT NULL PRIMARY KEY,
+	nombre 		VARCHAR(50) NOT NULL,
+	apellido 	VARCHAR(50) NOT NULL,
+	email		VARCHAR(50) NOT NULL,
+	password	VARCHAR(50) NOT NULL,
+	direccion	VARCHAR(50) NULL,
+	telefono	VARCHAR(10) NULL
+);
+
+
+-- 4. Tabla payment_method
+CREATE TABLE forma_pago(
+	id 			INT 		NOT NULL PRIMARY KEY,
+	forma_pago 	VARCHAR(50)	NOT NULL,
+	id_cliente	INT 		NULL
+);
+
+
+-- 5. Relaciones entre tablas
+ALTER TABLE forma_pago 
+	ADD FOREIGN KEY (id_cliente) 
+	REFERENCES cliente(id);
+
+/*La siguiente linea se agregao para poder generar el
+diagrama que se solicita en el Reto.
+	Esta sentencia crea un relacion entre tienda y pro_
+	ducto mediante la variable id_tienda en la tabla
+	producto, una realcion uno a muchos de tienda a
+	producto
+*/
+ALTER TABLE productos ADD id_tienda INT NOT NULL;
+
+ALTER TABLE productos 
+	ADD FOREIGN KEY (id_tienda)
+	REFERENCES tienda(id);
+    
+    
+-- 6. Tabla purchase_order
+CREATE TABLE orden_compra(
+	id 			INT 		NOT NULL PRIMARY KEY,
+    id_forma_pago 	INT		NOT NULL,
+    id_cliente 		INT		NOT NULL
+);
+ALTER TABLE orden_compra
+	ADD FOREIGN KEY (id_forma_pago)
+	REFERENCES forma_pago(id);
+ALTER TABLE orden_compra
+	ADD FOREIGN KEY (id_cliente)
+	REFERENCES cliente(id);
+
+
+CREATE TABLE detalle_orden_compra(
+	id 			INT 		NOT NULL PRIMARY KEY,
+	cantidad	INT 		NOT NULL,
+	precio_x_unidad DOUBLE	NOT NULL,
+	descripcion VARCHAR(150)	NULL,
+    id_producto 	INT 	NOT NULL,
+    id_orden_compra INT 	NOT NULL
+);
+ALTER TABLE  detalle_orden_compra ADD FOREIGN KEY 
+	(id_producto) REFERENCES productos(id);
+ALTER TABLE  detalle_orden_compra ADD FOREIGN KEY 
+	(id_orden_compra) REFERENCES orden_compra(id);
 
 
 -- 7. Revisión de estructura
